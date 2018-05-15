a54ce45ad5986a5b1766b0a750181850d75326e3
QiZhao <956361916@qq.com>
2018-05-15 16:00:36 +0800

    Add Party Planner.

---
+++ Party Planner/DinnerParty.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Party_Planner
+{
+    class DinnerParty
+    {
+        private const int CostOfFoodPersonb = 25;
+        private int NumberOfPeople=5;
+        private decimal CostOfBeveragesPerson;
+        private decimal CostOfDecorations = 0;
+        public void SetHealthyOption(bool healthyOption)
+        {
+            if (healthyOption)
+            {
+                CostOfBeveragesPerson = 5.00M;
+            }
+            else
+            {
+                CostOfBeveragesPerson = 20.00M;
+            }
+        }
+
+        public void CalculateCostOfDecorations(bool fancy)
+        {
+            if (fancy)
+            {
+                CostOfDecorations = (NumberOfPeople * 15.00M) + 50M;
+            }
+            else
+            {
+                CostOfDecorations = (NumberOfPeople * 7.50M) + 30M;
+            }
+        }
+
+        public decimal CalculateCost(bool healthyOption)
+        {
+            decimal totalCost = CostOfDecorations + ((CostOfBeveragesPerson + CostOfFoodPersonb) * NumberOfPeople);
+            if (healthyOption)
+            {
+                return totalCost * .95M;
+            }
+            else
+            {
+                return totalCost;
+            }
+        }
+        public void SetPartyOption(int people,bool fancy)
+        {
+            NumberOfPeople = people;
+            CalculateCostOfDecorations(fancy);
+        }
+        public int getNumberOfPeoiple()
+        {
+            return NumberOfPeople;
+        }
+    }
+}
---
+++ Party Planner/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Party_Planner
+{
+    public partial class Form1 : Form
+    {
+        DinnerParty dinnerParty;
+        public Form1()
+        {
+            InitializeComponent();
+            dinnerParty = new DinnerParty();
+            dinnerParty.CalculateCostOfDecorations(fancyBox.Checked);
+            dinnerParty.SetHealthyOption(healthyBox.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            int p = (int)numericUpDown1.Value;
+            dinnerParty.SetPartyOption(p, fancyBox.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void fancyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.CalculateCostOfDecorations(fancyBox.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void healthyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.SetHealthyOption(healthyBox.Checked);
+            DisplayDinnerPartyCost();
+        }
+        private void DisplayDinnerPartyCost()
+        {
+            decimal cost = dinnerParty.CalculateCost(healthyBox.Checked);
+            costLabel.Text = cost.ToString("c");
+        }
+
+    }
+}
